034377a93553c3f2199ae9887bed039665180e25
Felix Kopp <felix@phorax.com>
2013-03-14 16:33:21 +0100

    [TASK] Add phpunit container + travis script reference

---
+++ Tests/ProbeTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Class ProbeTest
+ */
+class ProbeTest extends PHPUnit_Framework_TestCase {
+
+	private $fixture;
+
+	public function setUp() {
+	}
+
+	public function tearDown() {
+	}
+
+}
+
+?>
